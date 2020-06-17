a0def8f1f23154644d8c7c190ecf9a3280c68bc4
Osgilia_ <henri.defrance62@gmail.com>
2020-06-17 18:37:55 +0200

    Fix: Instance Name duplication

--- src/dao/JpaInstanceDao.java
+++ src/dao/JpaInstanceDao.java
@@ -9,21 +9,23 @@ import modele.Instance;
  * @author Henri, Lucas, Louis
  */
 public class JpaInstanceDao extends JpaDao<Instance> implements InstanceDao {
+
     private static JpaInstanceDao instance;
-    
+
     /**
      * Default Constructor
      */
     private JpaInstanceDao() {
         super(Instance.class);
     }
-    
+
     /**
      * get instance
-     * @return 
+     *
+     * @return
      */
     public static JpaInstanceDao getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new JpaInstanceDao();
         }
         return instance;
@@ -63,43 +65,63 @@ public class JpaInstanceDao extends JpaDao<Instance> implements InstanceDao {
     public boolean create(Instance obj) {
         return super.create(obj); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     /**
      * find instance by name
+     *
      * @param name
-     * @return 
+     * @return
      */
     @Override
     public Instance findByName(String name) {
         Query query = this.getEm().createNamedQuery("Instance.findByName")
                 .setParameter("name", name);
-        
+
         return (Instance) query.getSingleResult();
     }
 
     /**
      * find instance by id
+     *
      * @param id
-     * @return 
+     * @return
      */
     @Override
     public Instance findById(Integer id) {
         Query query = this.getEm().createNamedQuery("Instance.findById")
                 .setParameter("id", id);
-        
-        return (Instance) query.getSingleResult();    }
-    
+
+        return (Instance) query.getSingleResult();
+    }
+
     /**
      * find instance by dataset
+     *
      * @param dataset
-     * @return 
+     * @return
      */
     @Override
     public Collection<Instance> findByDataset(String dataset) {
         Query query = this.getEm().createNamedQuery("Instance.findByDataset")
                 .setParameter("dataset", dataset);
-        
+
         return query.getResultList();
     }
-    
+
+    /**
+     * find instance by name
+     *
+     * @param name
+     * @return
+     */
+    @Override
+    public Instance findByNameAndDataSet(String name, String dataset) {
+        Query query = this.getEm().createNamedQuery("Instance.findByNameAndDataSet")
+                .setParameter("name", name)
+                .
+                setParameter("dataset", dataset);
+
+        return (Instance) query.getSingleResult();
+    }
+
 }
